61f81e6b7c95e0759ff7a68f465a18059bf2f02a
henry <zhr626@outlook.com>
2025-07-17 18:38:19 +0800

    Fix setuptools configuration and build process
    - Change license from TOML table to simple string format
    - Simplify setup.py to avoid pyproject.toml conflicts
    - Fix setuptools_scm configuration
    - Add proper packages configuration
    - Remove duplicate dependencies definitions

--- setup.py
+++ setup.py
@@ -4,22 +4,10 @@ setup(
     name='simplejsonspider',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    description='A simple package to crawl JSON APIs and save response to local files.',
-    author='Zeturn',
-    author_email='hollowdata@outlook.com',
     packages=find_packages(),
     install_requires=[
         'requests',
         'PyYAML',
     ],
     python_requires='>=3.6',
-    url='https://github.com/HungryZhao/simplejsonspider',
-    license='MIT',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
 )
